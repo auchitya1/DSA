84b48c3c323366b9aed34a0921adceec2a79041e
auchitya1 <104352338+auchitya1@users.noreply.github.com>
2022-06-18 21:14:25 +0530

    Add files via upload

---
+++ Ass 5 Sol..py
@@ -0,0 +1,91 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========== RESTART: C:/Users/Auchitya Sharma/OneDrive/Desktop/ASS 5.py =========
+ANS.1
+Enter the the string :Auchitya Sharma
+amrahS aytihcuA
+ANS.2
+Enter the minimum value :2
+Enter the maximum value: 80
+Enter the number for divisibility check :9
+9
+18
+27
+36
+45
+54
+63
+72
+ANS.3
+Enter the length of side:4
+Enter the length of side:5
+Enter the length of side:8
+8.181534085976786
+ANS.4
+Enter the no. of rows :9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+ANS.5
+Enter the number of rows5
+
+A
+BC
+DEF
+GHIJ
+KLMNO
+ANS.6
+Enter the Lowest Range Value : 1
+Enter the Upper Range Value : 50
+The Prime Numbers in this range are : 
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+ANS.7
+The numbers divisible by 11 and are multiple of 7 are : [77, 154, 231, 308, 385, 462]
+ANS.8
+Enter the number 8
+Enter the number 5
+Enter the number 2
+Enter the number -4
+Enter the number -6
+Enter the number -8
+Enter the number 2
+Enter the number 5
+Enter the number -6
+Enter the number -4
+Positive numbers are:  [8, 5, 2, 2, 5]
+Negative numbers are:  [-4, -6, -8, -6, -4]
+Even numbers are:  [8, 2, -4, -6, -8, 2, -6, -4]
+Odd numbers are:  [5, 5]
+Number of times each number occurs in the List {8: 1, 5: 2, 2: 2, -4: 2, -6: 2, -8: 1}
+ANS.9
+Number of words: 8
+Enter the word: Ant
+Enter the word: gate
+Enter the word: door
+Enter the word: lap
+Enter the word: gate
+Enter the word: Ant
+Enter the word: Auchitya
+Enter the word: Main
+Number of occurences:  {'Ant': 2, 'gate': 2, 'door': 1, 'lap': 1, 'Auchitya': 1, 'Main': 1}
