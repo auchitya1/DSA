d71941c4810ae6b243ed0c3477868b2aee2cb69e
auchitya1 <104352338+auchitya1@users.noreply.github.com>
2022-06-20 00:38:20 +0530

    Create assignment4sol.py

---
+++ assignment4sol.py
@@ -0,0 +1,41 @@
+#question1
+Enter your marks56
+C
+#question2
+Enter your year2002
+2002 is not a leap year
+#question3
+What is  3 times 8
+Enter your answer:24
+Correct
+What is  7 times 4
+Enter your answer:25
+That is incorrect  The correct answer is : 28
+What is  9 times 4
+Enter your answer:36
+Correct
+What is  1 times 7
+Enter your answer:7
+Correct
+What is  6 times 9
+Enter your answer:87
+That is incorrect  The correct answer is : 54
+What is  2 times 5
+Enter your answer:10
+Correct
+What is  5 times 0
+Enter your answer:0
+Correct
+What is  7 times 3
+Enter your answer:34
+That is incorrect  The correct answer is : 21
+What is  8 times 2
+Enter your answer:16
+Correct
+What is  5 times 7
+Enter your answer:67
+That is incorrect  The correct answer is : 35
+Thank you for playing
+#question4
+Number of candy are : 177
+Press any key to continue . . .
